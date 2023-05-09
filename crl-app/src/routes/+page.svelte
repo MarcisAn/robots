c4a8ae5547a2f8dcfc16c7605ee262691fd2f621
Mārcis <60743431+MarcisAn@users.noreply.github.com>
2023-05-09 15:17:07 +0300

    intial

---
+++ crl-app/src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<div>
+
+<div>
+    <h1>Modris pilnīgā kontrolē</h1>
+
+<div class="connect">
+    <label for="ip">Modra I.P. adrese</label>
+    <input id="ip" bind:value={ip}>
+    
+    <a href={"/panel?ip=" + ip}>Pievienoties</a>
+</div>
+</div>
+
+</div>
+<script>
+    export let ip = "";
+</script>
+
+<style>
+   
+h1{
+    color: white;
+    font-family: Arial;
+}
+.connect{
+color: white;
+}
+</style>
---
+++ crl-app/src/routes/panel/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+    import { page } from '$app/stores'
+    import { onMount } from 'svelte';
+
+console.log();
+let socket
+onMount(() => {
+  socket = new WebSocket("ws://"+$page.url.searchParams.get('ip'))
+  socket.addEventListener("open", ()=> {
+    console.log("Opened")
+  })
+})
+
+</script>
+
+<div>
+    
+</div>
