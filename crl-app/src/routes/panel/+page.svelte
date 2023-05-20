8c855634847973b10db438220145f42e628cdb6d
Mārcis <60743431+MarcisAn@users.noreply.github.com>
2023-05-20 08:38:52 +0300

    initial controll

--- crl-app/src/routes/panel/+page.svelte
+++ crl-app/src/routes/panel/+page.svelte
@@ -3,16 +3,67 @@
     import { onMount } from 'svelte';
 
 console.log();
-let socket
+/**
+     * @type {WebSocket}
+     */
+let socket;
 onMount(() => {
   socket = new WebSocket("ws://"+$page.url.searchParams.get('ip'))
   socket.addEventListener("open", ()=> {
     console.log("Opened")
   })
 })
+let enabled = false;
 
+/**
+     * @param {string} btn
+     */
+function run(btn) {
+  if (enabled){
+    console.log("stop");
+    enabled = false;
+  }
+  else{
+    console.log(btn);
+    socket.send(btn)
+    enabled = true;
+  }
+}
 </script>
 
-<div>
-    
+<div class="ctr">
+  <div class="kreis">
+    <button on:click={() => run("RL")}>Grieties pa kreisi</button>
+    <button on:click={() => run("ML")}>Braukt pa kreisi</button>
+  </div>
+    <div class="fb">
+      <button on:click={() => run("FW")}>Uz priekšu</button>
+      <button on:click={() => run("BW")}>Uz atpakaļu</button>
+    </div>
+    <div class="lab">
+      <button on:click={() => run("RR")}>Grieties pa labi</button>
+      <button on:click={() => run("MR")}>Braukt pa labi</button>
+    </div>
 </div>
+
+<style>
+  .fb{
+    display: flex;
+    flex-direction: column;
+    width: 90px;
+  }
+  .lab{
+    display: flex;
+    flex-direction: column;
+    width: 90px;
+  }
+  .kreis{
+    display: flex;
+    flex-direction: column;
+    width: 90px;
+  }
+  .ctr{
+    display: flex;
+    flex-direction: row;
+  }
+</style>
